6ad497da64945103cb8d1d2d7148c10e1eba212f
Yohay Halevi <yohaihalevi@gmail.com>
2026-06-11 12:14:11 +0300

    #2 MySQL connection

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
 
+from database import db_connection
+
 app = FastAPI()
 
 def main():
+    db_connection.create_tables()
     uvicorn.run("main:app", port=8000, reload=True)
 
 if __name__ == "__main__":
